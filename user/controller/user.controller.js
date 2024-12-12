86995efcd17bc425af1b55d5a3d6bbd48d197121
Manice18 <manishmanice2003@gmail.com>
2024-12-12 16:01:19 +0530

    add: user gateway

--- user/controller/user.controller.js
+++ user/controller/user.controller.js
@@ -24,9 +24,12 @@ module.exports.register = async (req, res) => {
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
+
+    delete user._doc.password;
+
     res.cookie("token", token);
 
-    res.send({ message: "User registered successfully" });
+    res.send({ token, newUser });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
@@ -50,9 +53,11 @@ module.exports.login = async (req, res) => {
       expiresIn: "1h",
     });
 
+    delete user._doc.password;
+
     res.cookie("token", token);
 
-    res.send({ message: "User logged in successfully" });
+    res.send({ token, user });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
--- user/middleware/authMiddleware.js
+++ user/middleware/authMiddleware.js
@@ -1,11 +1,18 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.models");
+const blacklisttokenModel = require("../models/blacklisttoken.model");
 
 module.exports.userAuth = async (req, res, next) => {
   try {
     const token = req.cookies.token || req.headers.authorization.split(" ")[1];
 
     if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const isBlacklisted = await blacklisttokenModel.find({ token });
+
+    if (isBlacklisted.length) {
       return res.status(401).json({ message: "Unauthorized" });
     }
 
